2033cfe4503f6f1aff43ab4a3ef5bd0dfd9cddf3
Anne Lißner <anne_lissner@gmx.net>
2021-09-11 13:12:08 +0200

    add classes

--- recipe.rb
+++ recipe.rb
@@ -1,7 +1,20 @@
-class Recipe
-  attr_reader :title
+require 'kramdown'
 
-  def initialize(title)
+class Recipe
+  attr_reader :title, :image_url, :description, :chef, :id, :tags
+
+  def initialize(title:, image_url:, description:, chef:, id:, tags:)
     @title = title
+    @image_url = image_url
+    @description = apply_markdown(description)
+    @chef = chef
+    @id = id
+    @tags = tags
+  end
+
+  private
+
+  def apply_markdown(text)
+    Kramdown::Document.new(text).to_html
   end
 end
